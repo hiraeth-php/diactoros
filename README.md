777436c0b332cf9a5f3a19516ab47b49a6a22196
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2019-03-25 21:58:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This package provides PSR-7 support for Hiraeth via Zend's Diactoros library.
---
+++ src/delegates/ServerRequestDelegate.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Hiraeth\Diactoros;
+
+use Hiraeth;
+use Zend\Diactoros;
+
+/**
+ *
+ */
+class ServerRequestDelegate implements Hiraeth\Delegate
+{
+	/**
+	 * Get the class for which the delegate operates.
+	 *
+	 * @static
+	 * @access public
+	 * @return string The class for which the delegate operates
+	 */
+	static public function getClass(): string
+	{
+		return Diactoros\ServerRequest::class;
+	}
+
+
+	/**
+	 * Get the instance of the class for which the delegate operates.
+	 *
+	 * @access public
+	 * @param Hiraeth\Broker $broker The dependency injector instance
+	 * @return object The instance of the class for which the delegate operates
+	 */
+	public function __invoke(Hiraeth\Broker $broker): object
+	{
+		$_SERVER['REMOTE_ADDR'] = trim($_SERVER['REMOTE_ADDR'], '[]');
+
+		return Diactoros\ServerRequestFactory::fromGlobals();
+	}
+}
